f71e6d7c8c6b1513915076924282856fde7a49bc
Meretor <mayo16h@ncssm.edu>
2015-10-27 13:30:23 -0400

    Dumb stuff

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-function start()
+var annoy=true;
+function setAnnoy()
 {
+    if annoy
+        {
+       // body.innerHTML="<input type='button' onclick='annoy=not(annoy);setAnnoy();'/>........."
+        }
+    else
+    {
+      // body.innerHTML="<input type='button' onclick='annoy=not(annoy);setAnnoy();'/>........."
+    }   
 }
