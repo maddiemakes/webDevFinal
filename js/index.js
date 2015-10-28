fe67c311eef737df2fc4203cdf9d9ab63fe49dfc
jwellstech <jwells.me@gmail.com>
2015-10-28 13:32:01 -0400

    got annoyance switch working

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-var isAnnoy = false;
-
+var isAnnoy = 0;
 
 function randInt(a, b) {
     return Math.floor((Math.random() * (b - a + 1)) + a);
@@ -14,25 +13,28 @@ function randomColor() {
     return ["#" + color.toString(16), "#" + cinverse.toString(16)];
 }
 
-function setAnnoy() {
+function annoy() {
     var bodyelem = document.getElementById("bodyelem");
     var pelem = document.getElementById("pelem");
-    if (isAnnoy) {
-        var rColor = randomColor();
-        bodyelem.style.background = rColor[0];
-        pelem.style.color = "blue";
-    } else {
-        bodyelem.style.background = "white";
-        pelem.style.color = "black";
-        // body.innerHTML="<input type='button' onclick='annoy=not(annoy);setAnnoy();'/>........."
+    var h1elem = document.getElementById("h1elem");
+    isAnnoy = (isAnnoy + 1)%5;
+    switch (isAnnoy) {
+        case 4:
+            var rColor = randomColor();
+            bodyelem.style.background = rColor[0];
+            pelem.style.color = rColor[1];
+            bodyelem.style.fontFamily = "\"Comic Sans MS\", cursive, sans-serif";
+            pelem.style.fontFamily = "\"Comic Sans MS\", cursive, sans-serif";
+            break;
+        case 3:
+        case 2:
+        case 1:
+            break;
+        case 0:
+            bodyelem.style.background = "white";
+            h1elem.style.fontFamily = "Caecilia, Times, serif";
+            pelem.style.color = "rgb(56,56,56)";
+            pelem.style.fontFamily = "Gotham, Helvetica, Arial, sans-serif";
+            break;
     }
-}
-
-function annoy() {
-    if (isAnnoy) {
-        isAnnoy = false;
-    } else {
-        isAnnoy = true;
-    }
-    setAnnoy();
 }
