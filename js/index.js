300a58ded836f600c72c08ba6203d692e3fb9435
jwellstech <jwells.me@gmail.com>
2015-10-28 14:09:36 -0400

    changes of annoyance level 1-3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 var isAnnoy = 0;
+var scroll = 0;
+var bodyelem = document.getElementById("bodyelem");
+var pelem = document.getElementById("pelem");
+var h1elem = document.getElementById("h1elem");
 
 function randInt(a, b) {
     return Math.floor((Math.random() * (b - a + 1)) + a);
@@ -13,6 +17,32 @@ function randomColor() {
     return ["#" + color.toString(16), "#" + cinverse.toString(16)];
 }
 
+
+//this code block adapted from http://www.javascriptkit.com/dhtmltutors/parallaxscrolling/
+/* code block begin */
+//if(scroll = 1) {
+//    var bodyelem = document.getElementById("bodyelem");
+//    window.requestAnimationFrame = window.requestAnimationFrame
+//     || window.mozRequestAnimationFrame
+//     || window.webkitRequestAnimationFrame
+//     || window.msRequestAnimationFrame
+//     || function(f){setTimeout(f, 1000/60)}
+//
+//    function backgroundScroll(){
+//     var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically 
+//     //bubble1.style.top = -scrolltop * .2 + 'px' // move bubble1 at 20% of scroll rate
+//     //bubble2.style.top = -scrolltop * .5 + 'px' // move bubble2 at 50% of scroll rate
+//     var rColor = randomColor();
+//     bodyelem.style.background = rColor[0];
+//    }
+//
+//    window.addEventListener('scroll', function(){ // on page scroll
+//     requestAnimationFrame(backgroundScroll) // call parallaxbubbles() on next available screen paint
+//    }, false)
+//}
+/* code block end */
+
+
 function annoy() {
     var bodyelem = document.getElementById("bodyelem");
     var pelem = document.getElementById("pelem");
@@ -20,15 +50,49 @@ function annoy() {
     isAnnoy = (isAnnoy + 1)%5;
     switch (isAnnoy) {
         case 4:
+            break;
+        case 3:
+            window.requestAnimationFrame = window.requestAnimationFrame
+             || window.mozRequestAnimationFrame
+             || window.webkitRequestAnimationFrame
+             || window.msRequestAnimationFrame
+             || function(f){setTimeout(f, 1000/60)}
+
+            function fontScroll(){
+             var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically 
+             var rColor = randomColor();
+             pelem.style.color = rColor[0];
+             rColor = randomColor();
+             h1elem.style.color = rColor[1];
+            }
+            
+            window.addEventListener('scroll', function(){ // on page scroll
+             requestAnimationFrame(fontScroll) // call parallaxbubbles() on next available screen paint
+            }, false)
+            break;
+        case 2:
+            h1elem.style.fontFamily = "\"Comic Sans MS\", cursive, sans-serif";
+            window.requestAnimationFrame = window.requestAnimationFrame
+             || window.mozRequestAnimationFrame
+             || window.webkitRequestAnimationFrame
+             || window.msRequestAnimationFrame
+             || function(f){setTimeout(f, 1000/60)}
+
+            function backgroundScroll(){
+             var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically 
+             var rColor = randomColor();
+             bodyelem.style.background = rColor[0];
+            }
+
+            window.addEventListener('scroll', function(){ // on page scroll
+             requestAnimationFrame(backgroundScroll) // call parallaxbubbles() on next available screen paint
+            }, false)
+            break;
+        case 1:
             var rColor = randomColor();
             bodyelem.style.background = rColor[0];
             pelem.style.color = rColor[1];
-            bodyelem.style.fontFamily = "\"Comic Sans MS\", cursive, sans-serif";
             pelem.style.fontFamily = "\"Comic Sans MS\", cursive, sans-serif";
-            break;
-        case 3:
-        case 2:
-        case 1:
             break;
         case 0:
             bodyelem.style.background = "white";
